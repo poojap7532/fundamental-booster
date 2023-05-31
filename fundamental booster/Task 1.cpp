c41511c8fde45ca25b91be78127bb01f91c4748e
poojap7532 <130437269+poojap7532@users.noreply.github.com>
2023-05-31 07:32:43 -0700

    Add files via upload
    ..........fundamental booster.......

---
+++ fundamental booster/Task 1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+//1. WAP to check given input is numeric or not.
+using namespace std;
+
+int main(){
+	
+	char ch;
+	
+    cout<<"Enter value: ";
+	cin>>ch;
+	
+   if(ch >='0' && ch <= '9'){
+    	cout<<"numeric number"<<endl;
+} 
+   else{
+   	   cout<<"not numeric number"<<endl;
+}
+	
+	
+	return 0;
+}
